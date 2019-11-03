2bcfe1bc349746a82179f464cc5d40b16de7768a
Ash Allen <mail@ashallendesign.co.uk>
2019-11-03 15:09:35 +0000

    Apply fixes from StyleCI

--- tests/Unit/ConvertTest.php
+++ tests/Unit/ConvertTest.php
@@ -7,24 +7,20 @@ class ConvertTest extends TestCase
     /** @test */
     public function converted_value_for_today_is_returned_if_no_date_parameter_passed()
     {
-
     }
 
     /** @test */
     public function converted_value_in_the_past_is_returned_if_date_parameter_passed()
     {
-
     }
 
     /** @test */
     public function exception_is_thrown_if_the_from_parameter_is_invalid()
     {
-
     }
 
     /** @test */
     public function exception_is_thrown_if_the_to_parameter_is_invalid()
     {
-
     }
 }
--- tests/Unit/ExchangeRateBetweenDateRangeTest.php
+++ tests/Unit/ExchangeRateBetweenDateRangeTest.php
@@ -7,36 +7,30 @@ class ExchangeRateBetweenDateRangeTest extends TestCase
     /** @test */
     public function exchange_rates_between_date_range_are_returned()
     {
-
     }
 
     /** @test */
     public function exception_is_thrown_if_the_date_parameter_passed_is_in_the_future()
     {
-
     }
 
     /** @test */
     public function exception_is_thrown_if_the_end_date_parameter_passed_is_in_the_future()
     {
-
     }
 
     /** @test */
     public function exception_is_thrown_if_the_end_date_is_before_the_start_date()
     {
-
     }
 
     /** @test */
     public function exception_is_thrown_if_the_from_parameter_is_invalid()
     {
-
     }
 
     /** @test */
     public function exception_is_thrown_if_the_to_parameter_is_invalid()
     {
-
     }
 }
