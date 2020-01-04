1271f7c78e953da8a5c1b17ac9e5646f55bc2acf
Ashley Allen <mail@ashallendesign.co.uk>
2020-01-04 01:40:44 +0000

    Added docblocks to the ExchangeRate facade.

--- src/Facades/ExchangeRate.php
+++ src/Facades/ExchangeRate.php
@@ -2,8 +2,19 @@
 
 namespace AshAllenDesign\LaravelExchangeRates\Facades;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array currencies(array $currencies = [])
+ * @method static string exchangeRate(string $from, string $to, ?Carbon $date = null)
+ * @method static array exchangeRateBetweenDateRange(string $from, string $to, Carbon $date, Carbon $endDate, array $conversions = [])
+ * @method static float convert(int $value, string $from, string $to, Carbon $date = null)
+ * @method static float convertBetweenDateRange(int $value, string $from, string $to, Carbon $date, Carbon $endDate, array $conversions = [])
+ * @method static self shouldBustCache(bool $bustCache = true)
+ *
+ * @see \AshAllenDesign\LaravelExchangeRates\Classes\ExchangeRate
+ */
 class ExchangeRate extends Facade
 {
     protected static function getFacadeAccessor()
