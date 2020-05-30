2f8f78e53055dd661e013e5c3e1cae4af9bf7b9a
Ashley Allen <mail@ashallendesign.co.uk>
2020-05-30 01:06:31 +0100

    Updated comments.

--- src/Classes/ExchangeRate.php
+++ src/Classes/ExchangeRate.php
@@ -244,6 +244,15 @@ class ExchangeRate
         return $conversions;
     }
 
+    /**
+     * Determine whether if the cached result (if it
+     * exists) should be deleted. This will force
+     * a new exchange rate to be fetched from
+     * the API.
+     *
+     * @param  bool  $bustCache
+     * @return $this
+     */
     public function shouldBustCache(bool $bustCache = true): self
     {
         $this->shouldBustCache = $bustCache;
@@ -251,6 +260,15 @@ class ExchangeRate
         return $this;
     }
 
+    /**
+     * Attempt to fetch an item (more than likely an
+     * exchange rate) from the cache. If it exists,
+     * return it. If it has been specified, bust
+     * the cache.
+     *
+     * @param  string  $cacheKey
+     * @return mixed
+     */
     private function attemptToResolveFromCache(string $cacheKey)
     {
         if ($this->shouldBustCache) {
