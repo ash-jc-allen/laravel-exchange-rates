8b4717baa23515c5119310d23858962e237f5b33
Ash Allen <mail@ashallendesign.co.uk>
2023-01-28 19:52:00 +0000

    Removed unneeded constructor.

--- src/Classes/ExchangeRate.php
+++ src/Classes/ExchangeRate.php
@@ -6,17 +6,10 @@ namespace AshAllenDesign\LaravelExchangeRates\Classes;
 
 use AshAllenDesign\LaravelExchangeRates\Drivers\ExchangeRatesApILegacy\ExchangeRatesApiLegacyDriver;
 use AshAllenDesign\LaravelExchangeRates\Interfaces\ExchangeRateDriver;
-use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\Manager;
 
 class ExchangeRate extends Manager
 {
-    public function __construct(Container $container = null)
-    {
-        // TODO Is this safe to do?
-        parent::__construct($container ?? app(Container::class));
-    }
-
     public function createExchangeRatesApiLegacyDriver(): ExchangeRateDriver
     {
         return new ExchangeRatesApiLegacyDriver();
