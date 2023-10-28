3d31789bd56702fb225034c374f5fb509570198f
Ash Allen <mail@ashallendesign.co.uk>
2023-10-28 11:36:41 +0100

    Style updates.

--- src/Drivers/ExchangeRateHost/RequestBuilder.php
+++ src/Drivers/ExchangeRateHost/RequestBuilder.php
@@ -23,6 +23,7 @@ class RequestBuilder implements RequestSender
     public function makeRequest(string $path, array $queryParams = []): ResponseContract
     {
         $protocol = config('laravel-exchange-rates.ssl') ? 'https://' : 'http://';
+
         $rawResponse = Http::baseUrl($protocol.self::BASE_URL)
             ->get($path, $queryParams)
             ->throw()
